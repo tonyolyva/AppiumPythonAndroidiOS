5437b0b8ab3b24f255d26b0609da29bacc07c608
tonyolyva <olyvatony@gmail.com>
2026-03-10 15:33:54 -0700

    added neg test for iOS

--- tests/android/test_calculator.py
+++ tests/android/test_calculator.py
@@ -3,14 +3,39 @@ from appium.webdriver.common.appiumby import AppiumBy
 
 pytestmark = pytest.mark.android
 
-@pytest.mark.android
+# @pytest.mark.android
 def test_add(driver):
     driver.find_element(AppiumBy.ID, "com.google.android.calculator:id/digit_3").click()
     driver.find_element(AppiumBy.ID, "com.google.android.calculator:id/op_add").click()
     driver.find_element(AppiumBy.ID, "com.google.android.calculator:id/digit_2").click()
     driver.find_element(AppiumBy.ID, "com.google.android.calculator:id/eq").click()
 
-    actual = driver.find_element(AppiumBy.ID, "com.google.android.calculator:id/result_final").text
+    result_element = driver.find_element(AppiumBy.ID, "com.google.android.calculator:id/result_final")
+    actual = result_element.text
+
     assert actual == "5"
+
+# @pytest.mark.android
+def test_subtraction_inp_pos_out_pos(driver):
+    # driver.find_element(AppiumBy.ID, "com.google.android.calculator:id/op_sub").click()
+    driver.find_element(AppiumBy.ID, "com.google.android.calculator:id/digit_3").click()
+    driver.find_element(AppiumBy.ID, "com.google.android.calculator:id/op_sub").click()
+    driver.find_element(AppiumBy.ID, "com.google.android.calculator:id/digit_2").click()
+    driver.find_element(AppiumBy.ID, "com.google.android.calculator:id/eq").click()
+
+    result_element = driver.find_element(AppiumBy.ID, "com.google.android.calculator:id/result_final")
+    # actual = driver.find_element(AppiumBy.ID, "com.google.android.calculator:id/result_final")
+    actual = result_element.text
+    # print(f"================ actual:{actual} ====================")
+
+    # assert actual.replace('-', '−') == '−1'
+    # assert actual == '−1'
+    # assert actual == '-1'
+    assert actual == '1'
+
+
+
+
+
 
 
--- tests/ios/test_calculmath.py
+++ tests/ios/test_calculmath.py
@@ -11,3 +11,24 @@ def test_add(driver):
 
     actual = driver.find_element(AppiumBy.ACCESSIBILITY_ID, "displayValue").text
     assert actual == "5"
+
+
+def test_subtraction_inp_pos_out_pos(driver):
+    driver.find_element(AppiumBy.ACCESSIBILITY_ID, "3").click()
+    driver.find_element(AppiumBy.ACCESSIBILITY_ID, "-").click()
+    driver.find_element(AppiumBy.ACCESSIBILITY_ID, "2").click()
+    driver.find_element(AppiumBy.ACCESSIBILITY_ID, "=").click()
+
+    actual = driver.find_element(AppiumBy.ACCESSIBILITY_ID, "displayValue").text
+    assert actual == "1"
+
+
+def test_multiplication_fail(driver):
+    driver.find_element(AppiumBy.ACCESSIBILITY_ID, "3").click()
+    driver.find_element(AppiumBy.ACCESSIBILITY_ID, "*").click()
+    driver.find_element(AppiumBy.ACCESSIBILITY_ID, "0").click()
+    driver.find_element(AppiumBy.ACCESSIBILITY_ID, "=").click()
+
+    actual = driver.find_element(AppiumBy.ACCESSIBILITY_ID, "displayValue").text
+    assert actual == "3"    
+
